ddc96877d6e6c94ddbcea8000513df7c6f28ecc2
asib27 <asib.rahman1927@gmail.com>
2022-05-10 00:57:13 +0800

    finish ex-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let c = (far-32) * 5.0 / 9
+  return Math.round(c * 10) / 10.0;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let f = cel * 9.0 / 5 + 32
+  return Math.round(f * 10) / 10.0;
 };
 
 // Do not edit below this line
