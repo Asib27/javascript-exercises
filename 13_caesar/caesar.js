7263696c2d1b77bb496dd09993de6e36eea615ee
asib27 <asib.rahman1927@gmail.com>
2022-05-10 02:54:22 +0800

    finish ex-13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
+const caesar = function(text, shift) {
+    return text.split("").
+    map(code => {
+        let isUpper = code.match(/[A-Z]/);
+        code = code.toLowerCase();
 
+        if(!code.match(/[a-z]/)) return code;
+
+        code = ((code.charCodeAt(0) - 'a'.charCodeAt(0) + 26 + shift) % 26) + 'a'.charCodeAt(0);
+
+        if(isUpper)
+            return String.fromCharCode(code).toUpperCase();
+        else
+            return String.fromCharCode(code);
+    }).join("");
 };
 
 // Do not edit below this line
