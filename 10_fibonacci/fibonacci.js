92a41c7b7703d0ad99393731cef6ae6c84ba295b
asib27 <asib.rahman1927@gmail.com>
2022-05-10 02:04:09 +0800

    finish ex-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
 
+    if(num < 0) return "OOPS";
+    
+    let cur = 1, prev = 0;
+    for(let i = 1; i < num; i++){
+        [cur, prev] = [cur+prev, cur]
+    }
+    return cur;
 };
 
 // Do not edit below this line
