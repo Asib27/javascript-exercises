6e31df982f02e76f20cbb5f3f229cffe41c87cea
asib27 <asib.rahman1927@gmail.com>
2022-05-10 01:09:32 +0800

    finish ex-08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((s, cur)=> s+cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((pro, el) => pro * el, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let pro = 1;
+  for(let i = 0; i < b; i++)
+    pro *= a;
+  return pro;
 };
 
-const factorial = function() {
-	
+const factorial = function(b) {
+	let pro = 1;
+  for(let i = 0; i < b; i++)
+    pro *= i+1;
+  return pro;
 };
 
 // Do not edit below this line
