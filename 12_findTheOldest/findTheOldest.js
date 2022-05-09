94f118cff6069fcc0307aedadd13816cc464ba19
asib27 <asib.rahman1927@gmail.com>
2022-05-10 02:30:29 +0800

    finish ex-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.map(person=>{
+        if(person.hasOwnProperty('yearOfDeath')){
+            let age = person.yearOfDeath - person.yearOfBirth;
+            return {name: person.name, age: age};
+        }
+        else{
+            let age = (new Date()).getFullYear() - person.yearOfBirth;
+            return {name: person.name, age: age};
+        }
+    }).reduce((mx, per)=> per.age > mx.age? per: mx, {age: 0});
 };
 
 // Do not edit below this line
