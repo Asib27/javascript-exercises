d6e2d2253c1a27006b2b64d217b4bf654dd5a730
asib27 <asib.rahman1927@gmail.com>
2022-05-10 01:36:44 +0800

    finish ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let i = 0, j = str.length;
+    str = str.toLowerCase();
 
+    while(i <= j){
+        while(i < str.length && !str.charAt(i).match(/[a-z]/i)) i++;
+        while(j >= 0 && !str.charAt(j).match(/[a-z]/i)) j--;
+
+        if(i <= j && str.charAt(i) !== str.charAt(j))
+            return false;
+        i++;
+        j--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
