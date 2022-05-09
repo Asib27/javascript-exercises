8743facf55441a42878d29a2a85a89d05be607d3
asib27 <asib.rahman1927@gmail.com>
2022-05-10 00:40:48 +0800

    finish ex-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if(typeof(start) !== 'number' || typeof(finish) !== 'number')
+        return 'ERROR';
+    
+    if(start < 0 || finish < 0) return 'ERROR';
+    if(start > finish){
+        [start, finish] = [finish, start];
+    }
 
+    let sum = 0;
+    for(let i = start; i <= finish; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
